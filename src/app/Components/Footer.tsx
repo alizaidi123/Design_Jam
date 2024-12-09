a95ff3e11030e6b73e835699ba3bba4e7c692025
alizaidi123 <159936656+alizaidi123@users.noreply.github.com>
2024-12-10 00:59:31 +0500

    Footer.tsx

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -1,47 +1,60 @@
 export default function Footer() {
-    return (
-      <>
-        <footer>
-          <div className="footer p-5 border-t-2 leading-10 border-gray-300">
-            <div className="w-full sm:w-[14rem]">
-              <h2 className="font-bold text-[2rem]">Funiro.</h2>
-              <p className="leading-none">400 University Drive Suite 200 Coral Gables, FL 33134 USA</p>
-            </div>
-  
-            <div className="mt-6 sm:mt-0">
-              <h4 className="font-semibold">Navbar</h4>
-              <ul>
-                <li>Home</li>
-                <li>Shop</li>
-                <li>About</li>
-                <li>Contact</li>
-              </ul>
-            </div>
-  
-            <div className="mt-6 sm:mt-0">
-              <h4 className="font-semibold">Help</h4>
-              <ul>
-                <li>Payment Option</li>
-                <li>Returns</li>
-                <li>Privacy Policies</li>
-              </ul>
-            </div>
-  
-            <div className="mt-6 sm:mt-0">
-              <h4 className="font-semibold">Newsletter</h4>
-              <input type="text" placeholder="Your Email" className="w-full sm:w-auto mb-2 p-2 border-2 border-gray-300 rounded-md" />
-              <button type="submit" className="font-bold bg-[#B88E2F] text-white px-4 py-2 rounded">
+  return (
+    <>
+      <footer className="bg-white">
+        <div className="footer p-5 border-t-2 leading-10 border-gray-300 flex flex-col lg:flex-row lg:justify-between gap-8">
+          
+          <div className="w-full sm:w-auto">
+            <h2 className="font-bold text-2xl">Funiro.</h2>
+            <p className="leading-none">
+              400 University Drive Suite 200 Coral Gables, FL 33134 USA
+            </p>
+          </div>
+
+          
+          <div>
+            <h4 className="font-semibold">Navbar</h4>
+            <ul className="space-y-1">
+              <li>Home</li>
+              <li>Shop</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+
+          <div>
+            <h4 className="font-semibold">Help</h4>
+            <ul className="space-y-1">
+              <li>Payment Option</li>
+              <li>Returns</li>
+              <li>Privacy Policies</li>
+            </ul>
+          </div>
+
+          <div>
+            <h4 className="font-semibold">Newsletter</h4>
+            <div className="flex flex-col sm:flex-row items-center sm:items-start gap-2">
+              <input
+                type="text"
+                placeholder="Your Email"
+                className="w-full sm:w-auto p-2 border-2 border-gray-300 rounded-md"
+              />
+              <button
+                type="submit"
+                className="font-bold bg-[#B88E2F] text-white px-4 py-2 rounded w-full sm:w-auto"
+              >
                 SUBSCRIBE
               </button>
             </div>
           </div>
-  
-          <div className="para text-center mt-6">
-            <hr />
-            <p>2023 Funiro. All rights reserved</p>
-          </div>
-        </footer>
-      </>
-    );
-  }
-  
+        </div>
+
+      
+        <div className="text-center mt-6">
+          <hr className="mb-4" />
+          <p>2023 Funiro. All rights reserved</p>
+        </div>
+      </footer>
+    </>
+  );
+}
